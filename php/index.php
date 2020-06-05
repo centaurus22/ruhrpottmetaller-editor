2fce2ebe5f5ebea713dd01d67345d6447523034f
Thorres <thorres@brothersofgrey.net>
2020-06-06 00:59:48 +0200

    Add content to index.php
    The index.php loads the classes, forwards the request to the controller
    and displays the output.

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Include the classes.
+include('classes/controller.php');
+include('classes/model_concert.php');
+include('classes/model_band.php');
+include('classes/model_city.php');
+include('classes/model_venue.php');
+include('classes/model_pref.php');
+include('classes/view.php');
+
+// Merge $_GET und $_POST
+$request = array_merge($_GET, $_POST);
+// Create controller class
+$controller = new Controller($request);
+// Display the output of web application.
+echo $controller->display();
+
+?>
