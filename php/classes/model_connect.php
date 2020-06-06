7d165e1ce84f450a9eed188667e991a2128264c3
Thorres <thorres@brothersofgrey.net>
2020-06-06 22:24:15 +0200

    Correct error in ConnectModel

--- php/classes/model_connect.php
+++ php/classes/model_connect.php
@@ -1,15 +1,20 @@
 <?php
 
 class connectModel {
-	$dbhost = '';
-	$dbuser = '';
-	$dbuserpass = '';
-
 	public static function db_connect() {
+		$dbhost = '';
+		$dbuser = '';
+		$dbuserpass = '';
+		$db="";
 		include(../db_preferences.inc.php);
 		$mysqli = new mysqli($dbhost, $dbuser, $dbuserpass, $db);
 		if ($mysqli->connect_error) {
 			die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		}
+		$query='SET NAMES "utf8" COLLATE "utf8_general_ci";';
+		$mysqli->query($query);
+		if ($mysqli->error) {
+			die('MySQL Error (' . $mysqli->errno . ') ' . $mysqli->error);
 		}
 		return $mysqli;
 	}
