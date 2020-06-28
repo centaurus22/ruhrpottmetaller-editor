92031872b5cd0c1710bb72718cb6bff20170529d
Thorres <thorres@brothersofgrey.net>
2020-06-28 15:21:29 +0200

    Add a template to display concerts
    This teplate displays all the concerts in array in a human readable list.
    If the arrays contains one concert it is suitable for the concert export
    on the default site. If it contains more concerts it can be used for the
    monthly export.

---
+++ php/templates/concert.inc.php
@@ -0,0 +1,31 @@
+<p>
+
+<?php
+foreach($this->_['concerts'] as $concert) {
+	//Build the list of bands
+	$bands = '';
+	foreach($concert['bands'] as $band) {
+		if ($band[1]) {
+			$bands = $bands . sprintf('%1$s (%2$s), ', 
+				htmlspecialchars($band[0], ENT_QUOTES),
+				htmlspecialchars($band[2], ENT_QUOTES));
+		}
+		else {
+			$bands = $bands . sprintf('%1$s, ', 
+				htmlspecialchars($band[0], ENT_QUOTES));
+		}
+	}
+	$bands = substr($bands, 0, -2);
+	echo '* '. $concert['date_human'] . ' ';
+	if ($concert['kname']) {
+		echo htmlspecialchars($concert['kname'], ENT_QUOTES) . ' im';
+	}
+	echo ' ' . htmlspecialchars($concert['lname'], ENT_QUOTES) . ', ' . 
+		htmlspecialchars($concert['sname'], ENT_QUOTES) . ": ";
+	echo $bands . '. ';	
+	echo htmlspecialchars($concert['url'], ENT_QUOTES);
+}
+
+?>
+
+</p>
