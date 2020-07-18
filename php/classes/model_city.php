22c6be3bc5859564beeb30cb4f264bf11e6a6f9a
Thorres <thorres@brothersofgrey.net>
2020-07-18 15:36:14 +0200

    Add descriptions to CityModel file
    * add a decription to the model
    * add descriptions to every class variable
    * add descriptions to every class function including parameters and
      return values.

--- php/classes/model_city.php
+++ php/classes/model_city.php
@@ -1,19 +1,35 @@
 <?php
 
+/**
+ * Class to acces and manipulate the data in the band table.
+ * Version 1.0.0
+ */
 class CityModel {
-	//Class to acces and maintain city data.
-
+	//Link identifier for the connection to the database
 	private $mysqli = NULL;
 
+	/**
+	 * Call the function which initialize the database connection and write the link
+	 * identifier into the class variable.
+	 */
 	public function __construct() {
 		$mysqli = ConnectModel::db_conncect();
 		$this->mysqli = $mysqli;
 	}
 
+	/**
+	 * Call the function which initialize the database connection and write the link
+	 * identifier into the class variable.
+	 */
 	public function __destruct() {
 		$this->mysqli->close;
 	}
 
+	/**
+	 * Get city data from the database
+	 *
+	 * @return array  Array with city data.
+	 */
 	public function getCities() {
 		$stmt = $this->mysqli->prepare('SELECT id, name FROM stadt');
 		$stmt->execute();
@@ -22,6 +38,12 @@ class CityModel {
 		return $result;
 	}
 
+	/**
+	 * Get city data from one city with the submitted id.
+	 *
+	 * @param id integer Id of the city.
+	 * @return array  Array with city data.
+	 */
 	public function getCity($id) {
 		$stmt = $this->mysqli->prepare('SELECT id, name FROM stadt WHERE id=?');
 		$stmt->bind_param('i', $id);
@@ -31,6 +53,12 @@ class CityModel {
 		return $result;
 	}
 
+	/**
+	 * Insert data about a city into the database
+	 *
+	 * @param string name Name of the city. 
+	 * @return integer Returns 1 for successful operation, -1 for an error.
+	 */
 	public function setCity($name) {
 		$stmt = $this->mysqli->prepare('INSERT INTO stadt SET name=?');
 		$stmt->bind_param('s', $name);
@@ -40,6 +68,13 @@ class CityModel {
 		return $result;
 	}
 
+	/**
+	 * Update city data in the database
+	 *
+	 * @param integer Id of the band which is updated.
+	 * @param string name Name of the city. 
+	 * @return integer Returns 1 for successful operation, 0 for a non-existent id, -1 for an error.
+	 */
 	public function updateCity($id, $name) {
 		$this->mysqli->prepare('UPDATE stadt SET name=? WHERE id=?');
 		$stmt->bind_param('si', $name, $id);
