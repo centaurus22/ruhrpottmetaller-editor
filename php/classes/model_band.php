d51fd34b4ca1d197f03788fd42f2a9c2eb46388f
Thorres <thorres@brothersofgrey.net>
2020-07-18 15:09:16 +0200

    Add description to ConcertModel class
    * add a decription to the model
    * add descriptions to every class variable
    * add descriptions to every class function including parameters and
      return values.

--- php/classes/model_band.php
+++ php/classes/model_band.php
@@ -1,19 +1,36 @@
 <?php
 
+/**
+ * Class to acces and manipulate the data in the band table.
+ * Version 1.0.0
+ */
 class BandModel {
-	//Class to acces and maintain bands.
-
+	//Link identifier for the connection to the database
 	private $mysqli = NULL;
 
+	/**
+	 * Call the function which initialize the database connection and write the link
+	 * identifier into the class variable.
+	 */
 	public function __construct() {
 		$mysqli = ConnectModel::db_conncect();
 		$this->mysqli = $mysqli;
 	}
 
+	/**
+	 * Close the database connection.
+	 */
 	public function __destruct() {
 		$this->mysqli->close;
 	}
 
+	/**
+	 * Get band data from the database
+	 *
+	 * @param initial string Initial letter of the band name in capital letters or an empty string for 
+	 * 	all bands or a lowecase s for bands witch names start with a special character.
+	 * @return array  Array with band data.
+	 */
 	public function getBands($initial) {
 		switch($initial) {
 		case '':
@@ -34,6 +51,12 @@ class BandModel {
 		return $result;
 	}
 
+	/**
+	 * Get band data from one band with the submitted id.
+	 *
+	 * @param id integer Id of the band.
+	 * @return array  Array with band data.
+	 */
 	public function getBand($id) {
 		$stmt = $this->mysqli->prepare('SELECT id, name, nazi FROM band WHERE id=?');
 		$stmt->bind_param('i', $id);
@@ -43,8 +66,15 @@ class BandModel {
 		return $result;
 	}
 
-	public function setBand($id, $name, $nazi) {
-		$stmt = $this->mysqli->prepare('INSERT INTO band SET name=?, nazi=? WHERE id=?');
+	/**
+	 * Insert a band into the database
+	 *
+	 * @param string name Name of the band. 
+	 * @param integer nazi Export status of the band. 0 -> exportable 1-> non-exportable
+	 * @return integer Returns 1 for successful operation, 0 for a non-existent id, -1 for an error.
+	 */
+	public function setBand($name, $nazi) {
+		$stmt = $this->mysqli->prepare('INSERT INTO band SET name=?, nazi=?');
 		$stmt->bind_param('sii', $name, $nazi, $id);
 		$stmt->execute();
 		$result = $stmt->affected_rows;
@@ -52,7 +82,15 @@ class BandModel {
 		return ($result);
 	}
 
-	public function updateBand() {
+	/**
+	 * Update band date in the database
+	 *
+	 * @param integer Id of the band which is updated.
+	 * @param string name Name of the band. 
+	 * @param integer nazi Export status of the band. 0 -> exportable 1-> non-exportable
+	 * @return integer Returns 1 for successful operation, 0 for a non-existent id, -1 for an error.
+	 */
+	public function updateBand($id, $name, $nazi) {
 		$stmt = $this->mysqli->prepare('UPDATE band SET name=?, nazi=? WHERE id=?');
 		$stmt->bind_param('sii', $name, $nazi, $id);
 		$stmt->execute();
