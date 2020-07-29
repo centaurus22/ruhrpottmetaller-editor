ec0e68dc1325173dc036ff0103efaf34e795d81e
Thorres <thorres@brothersofgrey.net>
2020-07-29 23:08:41 +0200

    Add description to PrefModel file
    * add a decription to the model
    * add descriptions to every class variable
    * add descriptions to every class function including parameters and
      return values.

--- php/classes/model_pref.php
+++ php/classes/model_pref.php
@@ -1,36 +1,63 @@
 <?php
 
+/**
+ * Class to access and manipulate the data in the preferences table.
+ * Version 1.0.0
+ */
 class PrefModel {
-	//Class to acces and maintain city data.
-
+	//Link identifier for the connection to the database
 	private $mysqli = NULL;
 
-
+	/**
+	 * Call the function which initialize the database connection and write the link
+	 * identifier into the class variable.
+	 */
 	public function __construct() {
 		$mysqli = ConnectModel::db_conncect();
 		$this->mysqli = $mysqli;
 	}
 
+	/**
+	 * Close the database connection.
+	 */
 	public function __destruct() {
 		$this->mysqli->close;
 	}
 
+	/**
+	 * Read the preferences from the database.
+	 *
+	 * @return array Array with preferences. If no preferences are set it returns an empty array. 
+	 */
 	public function getPref() {
-		$stmt = $this->mysqli->prepare('SELECT export_lang, header, footer FROM preferences');
+		$stmt = $this->mysqli->prepare('SELECT export_lang, header, footer FROM preferences WHERE id = 1');
 		$stmt->execute();
 		$result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 		$stmt->close;
 		return $result;
 	}
 	
+	/**
+	 * Read preferences about the export language from the database.
+	 *
+	 * @return array Array with language preferences. If no preferences are set it returns an empty array. 
+	 */
 	public function getPrefExportLang() {
-		$stmt = $this->mysqli->prepare('SELECT export_lang FROM preferences');
+		$stmt = $this->mysqli->prepare('SELECT export_lang FROM preferences WHERE id = 1');
 		$stmt->execute();
 		$result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 		$stmt->close;
 		return $result;
 	}
 
+	/**
+	 * Set the preferences in the database.
+	 *
+	 * @param string export_lang Language in which the concerts should be exported.
+	 * @param string header	Header of the monthly overview of concerts.
+	 * @param string footer	Footer of the monthly overview of concerts.
+	 * @return integer Integer with a value greater 1 for a succesfully write of preferences, 0 for no changes, -1 for an error.
+	 */
 	public function updatePref($export_lang, $header, $footer) {
 		$stmt = $this->mysqli->prepare('UPDATE preferences SET export_lang=?, heade=?, footer=?');
 		$stmt->bind_param('sss', $export_lang, $header, $footer);
