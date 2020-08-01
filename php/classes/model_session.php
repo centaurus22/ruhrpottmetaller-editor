e6157d9359ffc0666fb0adc24da100509b2c38c2
Thorres <thorres@brothersofgrey.net>
2020-08-01 12:00:05 +0200

    Add description to ConnectModel file * add a decription to the model * add descriptions to every class variable * add descriptions to every class function including parameters and   return values. * Check parameters submit error values as a return valiue if the are not suitable.

--- php/classes/model_session.php
+++ php/classes/model_session.php
@@ -1,7 +1,16 @@
 <?php
 
-//Class to access and manipulate data saved in the session
-public function __construct() {
+/**
+ * Class to access and manipulate data saved in the session
+ * Version 1.0.0
+ */
+class SessionModel {
+	/**
+	 * Initializes the session if it is not already initialized.
+	 *
+	 * @return integer 1 if a session is activated or was already activ, -1 if PHP sessions are disabled.
+	 */
+	public function __construct() {
 		switch(session_status()) {
 		case PHP_SESSION_DISABLED:
 			return -1;
@@ -14,195 +23,173 @@ public function __construct() {
 		}
 	}
 
-	private function initConcertDisplayStatus {
+	/**
+	 * Add an empty array to the PHP Session to save the status of the concert exports on the
+	 * default site (opened / closed) if it is not already available.
+	 *
+	 * @return integer Always 1.
+	 */
+	private function initConcertDisplayStatus() {
 		if(!isset($_SESSION['concert_display_status'])) {
 			$_SESSION['concert_display_status'] = array();
 		}
+		return 1;
 
 	}	
 
-	private function initLineUp {
+	/**
+	 * Add an empty array to the PHP Session to save the lineup in the concert editor if it is not
+	 * already available.
+	 *
+	 * @return integer Always 1.
+	 */
+	private function initLineUp() {
 		if(!isset($_SESSION['lineup'])) {
 			$_SESSION['lineup'] = array();
 		}
+		return 1;
 	}
 
+	/**
+	 * Check the export status of a concert.
+	 *
+	 * @param integer id Id of the concert which export status should be checked.
+	 * @return integer 1: Export status is open, 0: Export status is closed, -1 id is no integer.
+	 */
 	public function getConcertDisplayStatusEntry ($id) {
-		initConcertDisplayStatus();
-		if  (isset($_SESSION['concert_display_status']["$id"]) 
-			AND $_SESSION['concert_display_status']["$id"]) {
-			return 1;
-		} 
-		else {
-			return 0;
+		if (is_int($id)) {
+			initConcertDisplayStatus();
+			if  (isset($_SESSION['concert_display_status']["$id"]) 
+				AND $_SESSION['concert_display_status']["$id"]) {
+				return 1;
+			} 
+			else {
+				return 0;
 
+			}
 		}
+		else {
+			return -1;
+		}	
 	}
 
+	/**
+	 * Change the export status of a concert.
+	 *
+	 * @param integer id Id of the concert.
+	 * @return integer 1: The Id is an integer, -1: the id is no ingeger.
+	 */
 	public function changeConcertDisplayStatusEntry ($id) {
-		initConcertDisplayStatus();
-		if ($this->getConcertDisplayStatus ($id)) {
-			$_SESSION['concert_display_status']["$id"] = 0;
+		if (is_int($id)) {
+			initConcertDisplayStatus();
+			if ($this->getConcertDisplayStatus ($id)) {
+				$_SESSION['concert_display_status']["$id"] = 0;
+			}
+			else {
+				$_SESSION['concert_display_status']["$id"] = 1;
+			}
+			return 1;
 		}
 		else {
-			$_SESSION['concert_display_status']["$id"] = 1;
+			return -1;
 		}
-		return 1;
 	}
 
+	/**
+	 * Delete the array witch contains the export statuses of the concerts.
+	 *
+	 * @return integer Always 1.
+	 */
 	public function delConcertDisplayStatusEntry () {
-		initConcertDisplayStatus();
 		if (isset($_SESSION['concert_display_status'])) {
 			unset($_SESSION['concert_display_status']);
 		}
 		return 1;
 	}
 
+	/**
+	 * Add a new band to the lineup array.
+	 *
+	 * @param integer row Number of the row under which the new band is added.
+	 * @return integer 1: parameter is an integer, -1: parameter is no integer.
+	 */
 	public function setBandLineup($row){
-		initLineUp();
-		$band = array('first' => 0, 'band_id' => 0, 'addition' => $addition);
-		array_splice($_SESSION['lineup'], $row, 0, $band);
-	}
-
-	public function updateBandLineup($row, $first, $band_id, $addition){
-		initLineUp();
-		$_SESSION['lineup'][$row] = array('first' => $first,
-			'band_id' => $band_id, 'addition' => $addition);
-	}
-
-	public function delBandLineup($row) {
-		initLineUp();
-		array_splice($_SESSION['lineup'], $row, 1);
-	}
-
-	public function shiftBandLineup($row, $direction) {
-		initLineUp();
-		array_splice($_SESSION['lineup'], $row, 1);
-		$lenght_lineup = count($_SESSION['lineup']);
-		if ($lenght_lineup > 1) {
-			$band_tmp = $_SESSION['lineup'][$row];
-			if ($direction == "up" AND $row > 0) {
-				$_SESSION['lineup'][$row] = $_SESSION['lineup'][$row - 1];
-				$_SESSION['lineup'][$row - 1] = $band_tmp;
-			}
-			elseif ($direction == "down" AND $row < $lenght_lineup - 1) {
-				$_SESSION['lineup'][$row] = $_SESSION['lineup'][$row + 1];
-				$_SESSION['lineup'][$row + 1] = $band_tmp;
-
-			}
-		}
-	}
-
-
-}
-
-class SessionModel {
-	public static function startConcertDisplaySession() {
-		switch(session_status()) {
-		case PHP_SESSION_DISABLED:
-			return -1;
-			break;
-		case PHP_SESSION_NONE:
-			session_start();
-		case PHP_SESSION_ACTIVE:
-			if(!isset($_SESSION['concert_display_status'])) {
-				$_SESSION['concert_display_status'] = array();
-			}
+		if (is_int($row)) {
+			initLineUp();
+			$band = array('first' => 0, 'band_id' => 0, 'addition' => '');
+			array_splice($_SESSION['lineup'], $row, 0, $band);
 			return 1;
-		}
-	}
-	
-	public function getConcertDisplayStatus ($id) {
-		if (!$this->startConcertDisplaySession()) {
-			return -1;
-		}
-		if  (isset($_SESSION['concert_display_status']["$id"]) 
-			AND $_SESSION['concert_display_status']["$id"]) {
-			return 1;
-		} 
-		else {
-			return 0;
-
-		}
-	}
-
-	public function changeConcertDisplayStatus ($id) {
-		if (!$this->startConcertDisplaySession()) {
-			return -1;
-		}
-		if ($this->getConcertDisplayStatus ($id)) {
-			$_SESSION['concert_display_status']["$id"] = 0;
 		}
 		else {
-			$_SESSION['concert_display_status']["$id"] = 1;
-		}
-		return 1;
-	}
-
-	public function delConcertDisplayStatus () {
-		if (!$this->startConcertDisplaySession()) {
 			return -1;
 		}
-		if (isset($_SESSION['concert_display_status'])) {
-			unset($_SESSION['concert_display_status']);
-		}
-		return 1;
 	}
 
-	public function startBandsSession() {
-		switch(session_status()) {
-		case PHP_SESSION_DISABLED:
-			return -1;
-			break;
-		case PHP_SESSION_NONE:
-			session_start();
-		case PHP_SESSION_ACTIVE:
-			if(!isset($_SESSION['lineup'])) {
-				$_SESSION['lineup'] = array();
-			}
-			$this->lineup = $_SESSION['lineup'];
+	/**
+	 * Change information in a specific row of the lineup-.
+	 *
+	 * @param integer row Number of the row.
+	 * @param string first First charakter of the band name or '#' for a special charakter
+	 * @param integer band_id Band id.
+	 * @param string addition Additional information about this special appearance
+	 * @return integer 1: row and band_id parameter are integers, -1: one of those parameters are no integer.
+	 */
+	public function updateBandLineup($row, $first, $band_id, $addition){
+		if (is_int($row) AND is_int($band_id)) {
+			initLineUp();
+			$_SESSION['lineup'][$row] = array('first' => $first,
+				'band_id' => $band_id, 'addition' => $addition);
 			return 1;
 		}
-	}
-
-	public function setBandLineup($row){
-		if (!$this->startBandsSession()) {
+		else {
 			return -1;
 		}
-		$band = array('first' => 0, 'band_id' => 0, 'addition' => $addition);
-		array_splice($this->item, $row, 0, $band);
 	}
 
-	public function updateBandLineup($row, $first, $band_id, $addition){
-		if (!$this->startBandsSession()) {
-			return -1;
-		}
-		$this->lineup[$row] = array('first' => $first, 'band_id' => $band_id, 'addition' => $addition);
-	}
-
+	/**
+	 * Delete a band to the lineup array.
+	 *
+	 * @param integer row Number of the row which is deleted.
+	 * @return integer 1: parameter is an integer, -1: parameter is no integer.
+	 */
 	public function delBandLineup($row) {
-		if (!$this->startBandsSession()) {
-			return -1;
+		if (is_int($row)) {
+			initLineUp();
+			array_splice($_SESSION['lineup'], $row, 1);
+			return 1;
 		}
-		array_splice($this->lineup, $row, 1);
+		else {
+			return -1
+		}
 	}
 
+	/**
+	 * Shift a band up or down in the lineup.
+	 *
+	 * @param integer row Number of the row which is deleted.
+	 * @param string direction Direction in which the band is shifted.
+	 * @return integer 1: correct parameters, -1: wrong parameters.
+	 */
 	public function shiftBandLineup($row, $direction) {
-		if (!$this->startBandsSession()) {
-			return -1;
-		}
-		$lenght_lineup = count($this->lineup);
-		if ($lenght_lineup > 1) {
-			$band_tmp = $this->lineup[$row];
-			if ($direction == "up" AND $row > 0) {
-				$this->lineup[$row] = $this->lineup[$row - 1];
-				$this->lineup[$row - 1] = $band_tmp;
-			}
-			elseif ($direction == "down" AND $row < $lenght_lineup - 1) {
-				$this->lineup[$row] = $this->lineup[$row + 1];
-				$this->lineup[$row + 1] = $band_tmp;
+		if (is_int($row) OR ($direction != "up" AND $direction != "down")) {
+			initLineUp();
+			array_splice($_SESSION['lineup'], $row, 1);
+			$lenght_lineup = count($_SESSION['lineup']);
+			if ($lenght_lineup > 1) {
+				$band_tmp = $_SESSION['lineup'][$row];
+				if ($direction == "up" AND $row > 0) {
+					$_SESSION['lineup'][$row] = $_SESSION['lineup'][$row - 1];
+					$_SESSION['lineup'][$row - 1] = $band_tmp;
+				}
+				elseif ($direction == "down" AND $row < $lenght_lineup - 1) {
+					$_SESSION['lineup'][$row] = $_SESSION['lineup'][$row + 1];
+					$_SESSION['lineup'][$row + 1] = $band_tmp;
 
+				}
 			}
+		}
+		else {
+			return -1;
 		}
 	}
 }
