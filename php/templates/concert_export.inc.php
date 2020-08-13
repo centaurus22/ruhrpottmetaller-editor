bdb865dd253367b1a6618aeb58850f5245d87eb3
Thorres <thorres@brothersofgrey.net>
2020-08-13 10:03:45 +0200

    Implement export of all concerts in one month
    Also rename the concert template to concert_export to clarify the
    function of the template file.

--- php/templates/concert_export.inc.php
+++ php/templates/concert_export.inc.php
@@ -1,6 +1,11 @@
-<p>
-
 <?php
+
+if (isset($this->_['header'])) {
+	echo $this->_['month_changer'];
+	echo '<div id="inhalt" class="inhalt_small">';
+	echo nl2br(htmlspecialchars($this->_['header'], ENT_QUOTES));
+}
+
 foreach($this->_['concerts'] as $concert) {
 	//Build the list of bands
 	$bands = '';
@@ -16,16 +21,18 @@ foreach($this->_['concerts'] as $concert) {
 		}
 	}
 	$bands = substr($bands, 0, -2);
-	echo '* '. $concert['date_human'] . ': ';
+	echo '<p>* '. $concert['date_human'] . ': ';
 	if ($concert['kname']) {
 		echo htmlspecialchars($concert['kname'], ENT_QUOTES) . ', ';
 	}
 	echo ' ' . htmlspecialchars($concert['lname'], ENT_QUOTES) . ' in  ' . 
 		htmlspecialchars($concert['sname'], ENT_QUOTES) . ".<br/>";
 	echo $bands . '.<br/>';	
-	echo htmlspecialchars($concert['url'], ENT_QUOTES);
+	echo htmlspecialchars($concert['url'], ENT_QUOTES) . "</p>\n";
 }
 
+if (isset($this->_['header'])) {
+	echo nl2br(htmlspecialchars($this->_['footer'], ENT_QUOTES));
+	echo '</div>';
+}
 ?>
-
-</p>
