75a964fb6af0e48520bc1a0b56c2ab933abd7e19
Thorres <thorres@brothersofgrey.net>
2020-08-14 18:45:20 +0200

    Prevent output of concerts with non-export flag
    If the export site is selected and a non export-flag is set, the concert
    is not added to the export.

--- php/templates/concert_export.inc.php
+++ php/templates/concert_export.inc.php
@@ -7,34 +7,35 @@ if (isset($this->_['header'])) {
 }
 
 foreach($this->_['concerts'] as $concert) {
-	//Build the list of bands
-	$bands = '';
-	foreach($concert['bands'] as $band) {
-		if ($band['zusatz'] != '') {
-			$bands = $bands . sprintf('%1$s (%2$s), ', 
-				htmlspecialchars($band['name'], ENT_QUOTES),
-				htmlspecialchars($band['zusatz'], ENT_QUOTES));
+	if (!isset($this->_['header']) OR (isset($this->_['header']) AND !isset($concert['nazi']))) { 
+		//Build the list of bands
+		$bands = '';
+		foreach($concert['bands'] as $band) {
+			if ($band['zusatz'] != '') {
+				$bands = $bands . sprintf('%1$s (%2$s), ', 
+					htmlspecialchars($band['name'], ENT_QUOTES),
+					htmlspecialchars($band['zusatz'], ENT_QUOTES));
+			}
+			else {
+				$bands = $bands . sprintf('%1$s, ', 
+					htmlspecialchars($band['name'], ENT_QUOTES));
+			}
 		}
-		else {
-			$bands = $bands . sprintf('%1$s, ', 
-				htmlspecialchars($band['name'], ENT_QUOTES));
+		$bands = substr($bands, 0, -2);
+		echo '<p>* ';
+		if ($concert['ausverkauft'] == 1) {
+			echo '(ausverkauft) ';
+		}	
+		$concert['date_human'] . ': ';
+		if ($concert['kname']) {
+			echo htmlspecialchars($concert['kname'], ENT_QUOTES) . ', ';
 		}
+		echo ' ' . htmlspecialchars($concert['lname'], ENT_QUOTES) . ' in  ' . 
+			htmlspecialchars($concert['sname'], ENT_QUOTES) . ".<br/>";
+		echo $bands . '.<br/>';	
+		echo htmlspecialchars($concert['url'], ENT_QUOTES) . "</p>\n";
 	}
-	$bands = substr($bands, 0, -2);
-	echo '<p>* ';
-	if ($concert['ausverkauft'] == 1) {
-		echo '(ausverkauft) ';
-	}	
-	$concert['date_human'] . ': ';
-	if ($concert['kname']) {
-		echo htmlspecialchars($concert['kname'], ENT_QUOTES) . ', ';
-	}
-	echo ' ' . htmlspecialchars($concert['lname'], ENT_QUOTES) . ' in  ' . 
-		htmlspecialchars($concert['sname'], ENT_QUOTES) . ".<br/>";
-	echo $bands . '.<br/>';	
-	echo htmlspecialchars($concert['url'], ENT_QUOTES) . "</p>\n";
 }
-
 if (isset($this->_['header'])) {
 	echo nl2br(htmlspecialchars($this->_['footer'], ENT_QUOTES));
 	echo '</div>';
