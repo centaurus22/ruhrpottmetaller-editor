13e3d6b18b4159479c26ebe1cd318b5e57d9ad6b
Thorres <thorres@brothersofgrey.net>
2020-09-20 16:40:03 +0200

    Replace custom autoloader width composer
    The composer is a more modern approach.

--- src/includes/autoloader.php
+++
@@ -1,6 +0,0 @@
-<?php
-function custom_autoloader($class) {
-  include 'rpmetaller/' . $class . '.php';
-}
-
-spl_autoload_register('custom_autoloader');
--- src/index.php
+++ src/index.php
@@ -3,11 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include('include/autoloader.php');
+include 'vendor/autoload.php';
 
 // Merge $_GET und $_POST
 $request = array_merge($_GET, $_POST);
-// Create controller class
-$controller = new Controller($request);
+// Create controller object
+$controller = new rpmetaller\Controller($request);
 // Display the output of web application.
-echo $controller->display();
+echo $controller->getOutput();
