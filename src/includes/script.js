47a2082ab319131ed29643a7c026b731c872f0de
Klaus Quintern <thorres@brothersofgrey.net>
2021-01-23 16:13:57 +0100

    Remove unused js functions from script file

--- src/includes/script.js
+++ src/includes/script.js
@@ -84,8 +84,8 @@ function get_band_select_options(row) {
 		document.getElementById("band_id_" + row).innerHTML=xmlhttp.responseText;
 		}
 	  }
-	var datei = "index.php?special=lineup_sub&type=band_select_options&first_sign=" + first_sign + "&band_id=" +band_id;
-	xmlhttp.open("GET",datei,true);
+	var file = "index.php?special=lineup_sub&type=band_select_options&first_sign=" + first_sign + "&band_id=" +band_id;
+	xmlhttp.open("GET",file , true);
 	xmlhttp.send();
 }
 
@@ -109,8 +109,8 @@ function set_band_lineup(row) {
 			document.getElementById("lineup").innerHTML=xmlhttp.responseText;
 		}
 	}
-	var datei = "index.php?special=lineup&type=add&row=" + row;
-	xmlhttp.open("GET",datei,true);
+	var file = "index.php?special=lineup&type=add&row=" + row;
+	xmlhttp.open("GET",file,true);
 	xmlhttp.send();
 }
 
@@ -121,8 +121,8 @@ function del_band_lineup(row) {
 		document.getElementById("lineup").innerHTML=xmlhttp.responseText;
 		}
 	  }
-	var datei = "index.php?special=lineup&type=del&row=" + row;
-	xmlhttp.open("GET",datei,true);
+	var file = "index.php?special=lineup&type=del&row=" + row;
+	xmlhttp.open("GET",file,true);
 	xmlhttp.send();
 }
 
@@ -134,8 +134,8 @@ function shift_band_lineup(row, direction) {
     		document.getElementById("lineup").innerHTML=xmlhttp.responseText;
     	}
   	}
-	var datei = "index.php?special=lineup&type=shift&row=" + row + "&direction=" + direction;
-	xmlhttp.open("GET",datei,true);
+	var file = "index.php?special=lineup&type=shift&row=" + row + "&direction=" + direction;
+	xmlhttp.open("GET",file,true);
 	xmlhttp.send();
 }
 
@@ -143,8 +143,8 @@ function save_band_lineup(row, field) {
 	var value = document.getElementById(field + "_" + row).value;
 	value=encodeURIComponent(value);
 	var xmlhttp=new XMLHttpRequest();
-	var datei = "index.php?special=lineup&type=save&row=" + row + "&field=" + field + "&value=" + value;
-	xmlhttp.open("GET",datei,true);
+	var file = "index.php?special=lineup&type=save&row=" + row + "&field=" + field + "&value=" + value;
+	xmlhttp.open("GET", file,true);
 	xmlhttp.send();
 }
 
@@ -159,8 +159,8 @@ function display_concert(concert_id, concert_status) {
                 document.getElementById("concert_low_" + concert_id).innerHTML=xmlhttp.responseText;
             }
         }
-        var datei = "index.php?display=concert&display_id=" + concert_id;
-        xmlhttp.open("GET",datei,true);
+        var file = "index.php?display=concert&display_id=" + concert_id;
+        xmlhttp.open("GET",file,true);
         xmlhttp.send();
         image.src = "images/minus_small.png";
         concert_high.className = "concert_high_opened concert_" + concert_status;
@@ -171,155 +171,4 @@ function display_concert(concert_id, concert_status) {
         concert_high.className = "concert_high_closed concert_" + concert_status;
         concert_low.className = "concert_low_closed concert_" + concert_status;
     }
-}
-
-function get_band_table() {
-	var anfang = document.getElementById("band_anfang").value;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-		document.getElementById("data").innerHTML=xmlhttp.responseText;
-		}
-	  }
-	var datei = "get_band_table.php?anfang=" + anfang;
-	xmlhttp.open("GET",datei,true);
-	xmlhttp.send();
-}
-
-function datenuebernahme(pos, edit_id) {
-	var feld_id = pos + "_" + edit_id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(feld_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "daten_uebernahme.php?pos=" + pos + "&edit_id=" + edit_id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function band_uebernahme(pos, band_id, edit_id) {
-	var feld_id = "band_" + pos + "_" + edit_id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(feld_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "band_uebernahme.php?pos=" + pos + "&band_id=" + band_id + "&edit_id=" + edit_id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function reset_aenderung(edit_id) {
-	var feld_id = "todo_" + edit_id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(feld_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "reset_aenderung.php?edit_id=" + edit_id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function loeschen_aenderung(edit_id) {
-	var feld_id = "todo_" + edit_id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(feld_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "loeschen_aenderung.php?edit_id=" + edit_id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function speichern_aenderung(edit_id) {
-	var feld_id = "todo_" + edit_id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(feld_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "speichern_aenderung.php?edit_id=" + edit_id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function edit_blog_entry(id) {
-	var nachricht_id = "nachricht_" + id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(nachricht_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "edit_blog_entry.php?id=" + id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-
-function close_blog_entry(id) {
-	var nachricht_id = "nachricht_" + id;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById(nachricht_id).innerHTML=xmlhttp.responseText;
-    	}
-  	}
-	var datei = "blog_entry.php?id=" + id;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function save_blog_entry(id) {
-	var ueberschrift = document.getElementById('ueberschrift_' + id).value;
-	var text = document.getElementById('text_' + id).value;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
-  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			document.getElementById(nachricht_id).innerHTML=xmlhttp.responseText;
-		}
-  	}
-	var datei = "blog_entry.php?id=" + id + "&text=" + text + "&ueberschrift=" + ueberschrift;
-	xmlhttp.open("GET", datei, true);
-	xmlhttp.send();
-}
-
-function change_binary(table, field){
-	var xmlhttp=new XMLHttpRequest();
-	var datei = "change_binary.php?table=" + table + "&field=" + field;
-	xmlhttp.open("GET",datei,true);
-	xmlhttp.send();
-}
-
-function change_binary_row(table, field, id) {
-	var xmlhttp=new XMLHttpRequest();
-	var datei = "change_binary_row.php?table=" + table + "&field=" + field + "&id=" +id;
-	xmlhttp.open("GET",datei,true);
-	xmlhttp.send();
-}
-
-function change_value(table, field) {
-        var xmlhttp=new XMLHttpRequest();
-        const data = new FormData();
-        data.append("table", table);
-        data.append("field", field);
-        data.append("value_new", document.getElementById(field).value);
-        xmlhttp.open("POST","change_value.php",true);
-        xmlhttp.send(data);
-}
-
-function change_value_row(table, field, id) {
-	var xmlhttp=new XMLHttpRequest();
-	var value_new = document.getElementById(field + "_" +  id).value;
-	var datei = "change_value_row.php?table=" + table + "&field=" + field + "&id=" + id + "&value_new=" + value_new;
-	xmlhttp.open("GET",datei,true);
-	xmlhttp.send();
 }
