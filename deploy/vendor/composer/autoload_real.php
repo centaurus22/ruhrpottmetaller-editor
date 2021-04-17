894e691515a4949533f38bee5685859c1d4d17ff
Klaus Quintern <thorres@brothersofgrey.net>
2021-04-17 08:28:30 +0200

    Install phpunit as a unit test system.

--- deploy/vendor/composer/autoload_real.php
+++ deploy/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit274f7156f4adb8a1c2930ecad063a69e
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit274f7156f4adb8a1c2930ecad063a69e', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit274f7156f4adb8a1c2930ecad063a69e', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit274f7156f4adb8a1c2930ecad063a69e
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit274f7156f4adb8a1c2930ecad063a69e::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire274f7156f4adb8a1c2930ecad063a69e($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire274f7156f4adb8a1c2930ecad063a69e($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
