83cec265f7fa541b366800128ae587d0d641daa0
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-02-17 20:49:27 +0100

    Adapt the preferences model.
    Adapt the preferences model to the new database structure.

--- ruhrpottmetaller/ModelPreferences.php
+++ ruhrpottmetaller/ModelPreferences.php
@@ -14,8 +14,11 @@ class ModelPreferences
     public function getPreferences(): array
     {
         $mysqli = $this->mysqli;
-        $stmt = $mysqli->prepare('SELECT export_lang, header, footer
-            FROM preferences WHERE id = 1');
+        $stmt = $mysqli->prepare('
+            SELECT export_lang, export_header as header, export_footer as footer 
+            FROM preferences
+            WHERE id = 1
+        ');
         $stmt->execute();
         $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
         $stmt->close();
@@ -25,8 +28,7 @@ class ModelPreferences
     public function getPreferencesExportLang(): array
     {
         $mysqli = $this->mysqli;
-        $stmt = $mysqli->prepare('SELECT export_lang FROM preferences
-            WHERE id = 1');
+        $stmt = $mysqli->prepare('SELECT export_lang FROM preferences WHERE id = 1');
         $stmt->execute();
         $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
         $stmt->close();
@@ -36,7 +38,7 @@ class ModelPreferences
     public function updatePreferences(string $export_lang, string $header, string $footer): int
     {
         $mysqli = $this->mysqli;
-        $stmt = $mysqli->prepare('UPDATE preferences SET export_lang=?, header=?, footer=?');
+        $stmt = $mysqli->prepare('UPDATE preferences SET export_lang=?, export_header=?, export_footer=?');
         $stmt->bind_param('sss', $export_lang, $header, $footer);
         $stmt->execute();
         $result = $stmt->affected_rows;
