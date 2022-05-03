c79da2e25a46af36201507e11e726185e1d4049b
Klaus Thorres <ruhrpottmetaller@freenet.de>
2022-05-03 22:33:52 +0200

    Test if a type Error is thrown on wrong data type
    Test if a type Error is thrown on wrong id data type

--- tests/Controller/EventQueryControllerTest.php
+++ tests/Controller/EventQueryControllerTest.php
@@ -14,12 +14,28 @@ final class EventQueryControllerTest extends TestCase
     private \ruhrpottmetaller\Controller\EventQueryController $Controller;
 
     /**
-     * @covers \ruhrpottmetaller\AbstractEventController
+     * @covers \ruhrpottmetaller\Controller\AbstractEventController
+     * @covers \ruhrpottmetaller\Controller\EventQueryController
+     * @covers \ruhrpottmetaller\DataType\AbstractDataTypeValue
+     * @covers \ruhrpottmetaller\DataType\DataTypeInt
      */
     public function testShouldSetIdAndGetTheSameId(): void
     {
         $this->Controller = \ruhrpottmetaller\Controller\EventQueryController::new();
         $this->Controller->setId(DataTypeInt::new(23));
         $this->assertEquals(23, $this->Controller->getId()->get());
+    }
+
+    /**
+     * @covers \ruhrpottmetaller\Controller\EventQueryController
+     * @covers \ruhrpottmetaller\Controller\AbstractEventController
+     * @covers \ruhrpottmetaller\DataType\AbstractDataTypeValue
+     * @covers \ruhrpottmetaller\DataType\DataTypeString
+     */
+    public function testShouldThrowTypeErrorIfNoDataTypeIntIsSetToId(): void
+    {
+        $this->expectException(\TypeError::class);
+        $this->Controller = \ruhrpottmetaller\Controller\EventQueryController::new();
+        $this->Controller->setId(DataTypeString::new('Iron Maiden'));
     }
 }
