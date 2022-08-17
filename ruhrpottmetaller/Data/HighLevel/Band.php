369bd639b32be02770d5849db084a20d49458a9d
klaus <ruhrpottmetaller@freenet.de>
2022-08-17 19:36:30 +0200

    Implement the band object.

--- ruhrpottmetaller/Data/HighLevel/Band.php
+++ ruhrpottmetaller/Data/HighLevel/Band.php
@@ -2,7 +2,20 @@
 
 namespace ruhrpottmetaller\Data\HighLevel;
 
-class Band extends AbstractHighLevelDataObject implements \ruhrpottmetaller\Data\IDataObject
-{
+use ruhrpottmetaller\Data\IDataObject;
+use ruhrpottmetaller\Data\LowLevel\RmBool;
 
+class Band extends AbstractHighLevelDataObject implements IDataObject
+{
+    private RmBool $IsVisible;
+
+    public function setIsVisible(RmBool $IsVisible)
+    {
+        $this->IsVisible = $IsVisible;
+    }
+
+    public function getIsVisible(): RmBool
+    {
+        return $this->IsVisible;
+    }
 }
