8d1a700d31c00f6a0395997268fe56e531519d2b
klaus <ruhrpottmetaller@freenet.de>
2022-08-17 20:02:34 +0200

    Implement the venue object.

--- ruhrpottmetaller/Data/HighLevel/Venue.php
+++ ruhrpottmetaller/Data/HighLevel/Venue.php
@@ -2,7 +2,30 @@
 
 namespace ruhrpottmetaller\Data\HighLevel;
 
-class Venue extends AbstractHighLevelDataObject implements \ruhrpottmetaller\Data\IDataObject
-{
+use ruhrpottmetaller\Data\IDataObject;
+use ruhrpottmetaller\Data\LowLevel\RmBool;
 
+class Venue extends AbstractHighLevelDataObject implements IDataObject
+{
+    private City $City;
+    private RmBool $IsVisible;
+
+    public function setCity(City $City)
+    {
+        $this->City = $City;
+    }
+
+    public function getCity(): City
+    {
+        return $this->City;
+    }
+    public function setIsVisible(RmBool $IsVisible)
+    {
+        $this->IsVisible = $IsVisible;
+    }
+
+    public function getIsVisible(): RmBool
+    {
+        return $this->IsVisible;
+    }
 }
---
+++ tests/Data/HighLevel/VenueTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\ruhrpottmetaller\Data\HighLevel;
+
+use PHPUnit\Framework\TestCase;
+use ruhrpottmetaller\Data\HighLevel\City;
+use ruhrpottmetaller\Data\HighLevel\Venue;
+use ruhrpottmetaller\Data\LowLevel\RmInt;
+use ruhrpottmetaller\Data\LowLevel\RmString;
+use ruhrpottmetaller\Data\LowLevel\RmBool;
+
+final class VenueTest extends TestCase
+{
+    private \ruhrpottmetaller\Data\HighLevel\Venue $DataSet;
+
+    /**
+     * @covers \ruhrpottmetaller\Data\HighLevel\AbstractHighLevelDataObject
+     * @covers \ruhrpottmetaller\Data\HighLevel\Venue
+     * @covers \ruhrpottmetaller\Data\LowLevel\AbstractLowLevelDataObject
+     * @covers \ruhrpottmetaller\Data\LowLevel\RmString
+     */
+    public function testShouldSetNameAndGetSameNameBack(): void
+    {
+        $this->DataSet = Venue::new();
+        $this->DataSet->setName(RmString::new('Essen'));
+        $this->assertEquals(
+            'Essen',
+            $this->DataSet->getName()->get()
+        );
+    }
+
+    /**
+     * @covers \ruhrpottmetaller\Data\HighLevel\AbstractHighLevelDataObject
+     * @covers \ruhrpottmetaller\Data\HighLevel\Venue
+     * @covers \ruhrpottmetaller\Data\LowLevel\AbstractLowLevelDataObject
+     * @covers \ruhrpottmetaller\Data\LowLevel\RmInt
+     */
+    public function testShouldSetIdAndGetSameIdBack(): void
+    {
+        $this->DataSet = Venue::new();
+        $this->DataSet->setId(RmInt::new(123));
+        $this->assertEquals(
+            '123',
+            $this->DataSet->getId()->get()
+        );
+    }
+
+    /**
+     * @covers \ruhrpottmetaller\Data\HighLevel\AbstractHighLevelDataObject
+     * @covers \ruhrpottmetaller\Data\HighLevel\Venue
+     * @covers \ruhrpottmetaller\Data\LowLevel\AbstractLowLevelDataObject
+     * @covers \ruhrpottmetaller\Data\LowLevel\RmBool
+     */
+    public function testShouldSetIsVisibleAndGetSameIsVisibleValue(): void
+    {
+        $this->DataSet = Venue::new();
+        $this->DataSet->setIsVisible(RmBool::new(true));
+        $this->assertEquals(
+            true,
+            $this->DataSet->getIsVisible()->get()
+        );
+    }
+
+    /**
+     * @covers \ruhrpottmetaller\Data\HighLevel\AbstractHighLevelDataObject
+     * @covers \ruhrpottmetaller\Data\HighLevel\Venue
+     * @covers \ruhrpottmetaller\Data\LowLevel\AbstractLowLevelDataObject
+     * @covers \ruhrpottmetaller\Data\LowLevel\RmBool
+     * @covers \ruhrpottmetaller\Data\LowLevel\RmString
+     */
+    public function testShouldSetCityAndGetSameCityObject(): void
+    {
+        $this->DataSet = Venue::new();
+        $City = City::new()->setName(RmString::new('Duisburg'));
+        $this->DataSet->setCity($City);
+        $this->assertEquals(
+            'Duisburg',
+            $this->DataSet->getCity()->getName()->get()
+        );
+    }
+}
