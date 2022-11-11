1ffce99304a558d8161cc4b5d1ba58238d2cb8f9
klaus <ruhrpottmetaller@freenet.de>
2022-11-11 12:38:13 +0100

    Create the basic table design.

--- templates/event_main.inc.php
+++ templates/event_main.inc.php
@@ -1,9 +1,20 @@
+<?php
+
+use ruhrpottmetaller\Data\LowLevel\String\RmString;
+
+?>
 <div class="rm_table">
+    <div class="rm_table_header">
+        <?=RmString::new('Date')->asTableCell()?>
+        <?=RmString::new('Name')->asTableCell()?>
+        <?=RmString::new('Venue')->asTableCell()?>
+        <?=RmString::new('Url')->asTableCell()?>
+    </div>
     <?php while ($this->get('events')->hasCurrent()) : ?>
         <?php $event = $this->get('events')->getCurrent(); ?>
         <div class="rm_table_row">
-            <?=$event->getName()->asTableCell() ?>
             <?=$event->getFormattedDate()->asTableCell() ?>
+            <?=$event->getName()->asTableCell() ?>
             <?=$event->getVenue()->asVenueAndCity()->asTableCell()?>
             <?=$event->getUrl()->asWwwUrl()->asTableCell()?>
         </div>
