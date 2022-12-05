e802dc00a6c621488f3702f61213fbfd198d1157
klaus <ruhrpottmetaller@freenet.de>
2022-12-05 21:01:34 +0100

    Let every model method create its own array.
    Do not provide the Array, which eventually contains the data, by
    constructor. Instead, let every model method create the array
    by itself.

--- ruhrpottmetaller/Model/AbstractDatabaseModel.php
+++ ruhrpottmetaller/Model/AbstractDatabaseModel.php
@@ -9,14 +9,8 @@ abstract class AbstractDatabaseModel
     protected ?\mysqli $connection;
     protected ?RmArray $array;
 
-    public function __construct(?\mysqli $connection, ?RmArray $array)
+    public function __construct(?\mysqli $connection)
     {
         $this->connection = $connection;
-        $this->array = $array;
-    }
-
-    public static function new(?\mysqli $connection, ?RmArray $array)
-    {
-        return new static($connection, $array);
     }
 }
