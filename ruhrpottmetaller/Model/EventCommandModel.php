775ed4436dcde0d0f2e6ddce2b69331e54ea1fd0
klaus <ruhrpottmetaller@freenet.de>
2023-01-30 19:01:24 +0100

    Add an EventCommandModel.
    Add an EventCommandModel which allows updating the status of the sale.

---
+++ ruhrpottmetaller/Model/EventCommandModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ruhrpottmetaller\Model;
+
+use ruhrpottmetaller\Data\HighLevel\Venue;
+use ruhrpottmetaller\Data\LowLevel\Int\RmInt;
+
+class EventCommandModel extends AbstractCommandModel
+{
+    public static function new(?\mysqli $connection): EventCommandModel
+    {
+        return new static($connection);
+    }
+
+    public function setSoldOut(RmInt $eventId): void
+    {
+        $query = 'UPDATE event SET is_sold_out = 1 WHERE id = ?';
+        $this->query(
+            $query,
+            'i',
+            [
+                $eventId->get()
+            ]
+        );
+    }
+}
