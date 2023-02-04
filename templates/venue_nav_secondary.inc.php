8562e2ecb7efa81862cb0e2c59d28284ba9d8caf
klaus <ruhrpottmetaller@freenet.de>
2023-02-04 23:38:02 +0100

    Fix a bug, which lets the select elements change their height, if none is selected.

--- templates/venue_nav_secondary.inc.php
+++ templates/venue_nav_secondary.inc.php
@@ -8,7 +8,7 @@ use ruhrpottmetaller\Data\LowLevel\String\RmString;
     <?=RmString::new('venues')
         ->asHiddenInput(RmString::new('show')) ?>
     <label for="filter_by">City:</label> <select id="filter_by" name="filter_by">
-        <option value=""></option>
+        <option value="">&nbsp;</option>
         <?php while ($this->get('cities')->hasCurrent()) : ?>
             <?php $currentCity = $this->get('cities')->getCurrent()->getName()->get() ?>
             <?php if ($currentCity == $this->get('filterByParameter')) : ?>
