b87ef6f4bb18e9b69b279c755ee27a6c36a36c29
klaus <ruhrpottmetaller@freenet.de>
2023-02-05 14:23:57 +0100

    Mention the filter settings.
    Mention the filter settings in the templates which are rendered when no
    element was found.

--- templates/band_main_empty.inc.php
+++ templates/band_main_empty.inc.php
@@ -1,4 +1,4 @@
 <div id="text_content">
-    There are currently no bands defined in the system. They are automatically added when generating new
-    <a href="?show=events">events</a>.
+    No bands were found after applying the filter settings.
+    They are automatically added when generating new <a href="?show=events">events</a>.
 </div>
