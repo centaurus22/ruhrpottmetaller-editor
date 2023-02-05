3a66a47bada777bd8cacbc76d075bf30877943b5
klaus <ruhrpottmetaller@freenet.de>
2023-02-05 12:14:08 +0100

    Improve the styling of the event page.

--- templates/event_main.inc.php
+++ templates/event_main.inc.php
@@ -15,22 +15,23 @@ use ruhrpottmetaller\Data\LowLevel\String\RmString;
     <?php while ($this->get('events')->hasCurrent()) : ?>
         <?php $event = $this->get('events')->getCurrent(); ?>
         <form action="" class="rm_table_row">
+            <?=$event->getFormattedDate()->asTableCell() ?>
+            <?=$event->getName()->asTableCell() ?>
+            <?=$event->getVenueAndCityName()->asTableCell()?>
+            <?=$event->getBandList()->asTableCell()?>
+            <?=$event->getUrl()->asWwwUrl()->asTableCell()?>
+            <?=$event->getId()->asHiddenInput(RmString::new('id'))?>
+            <?=$event->getDate()->getFormatted()->asHiddenInput(RmString::new('date'))?>
             <?=RmString::new('events')
                 ->asHiddenInput(RmString::new('show')) ?>
             <?=RmString::new('event')
                 ->asHiddenInput(RmString::new('modify')) ?>
             <?=$this->get('filterByParameter')
                 ->asHiddenInput(RmString::new('filter_by')) ?>
-            <?=$event->getId()->asHiddenInput(RmString::new('id'))?>
-            <?=$event->getDate()->getFormatted()->asHiddenInput(RmString::new('date'))?>
-            <?=$event->getFormattedDate()->asTableCell() ?>
-            <?=$event->getName()->asTableCell() ?>
-            <?=$event->getVenueAndCityName()->asTableCell()?>
-            <?=$event->getBandList()->asTableCell()?>
-            <?=$event->getUrl()->asWwwUrl()->asTableCell()?>
             <?=RmString::new('<select name="action">
                 <option value="add">Add</option>
                 <option value="edit">Edit</option>
+                <option value="canceled">Set canceled</option>
                 <option value="delete">Delete</option>
                 <option value="sold_out">Set sold out</option>
             </select>')->concatWith(RmString::new('Do It')->asSubmitButton())->asTableCell()?>
