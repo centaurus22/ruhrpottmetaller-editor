62e2b9c2050895238ecebead7656570c4232f1f1
Klaus Thorres <thorres@brothersofgrey.net>
2023-11-27 10:10:18 +0100

    Add ids to the lineup buttons

--- templates/ajax/editor_lineup.inc.php
+++ templates/ajax/editor_lineup.inc.php
@@ -9,7 +9,7 @@ $lineupIndex = 1;
 
 ?>
 
-<?php while ($this->get('gigs')->hasCurrent()): ?>
+<?php while ($this->get('gigs')->hasCurrent()) : ?>
     <?php $gig = $this->get('gigs')->getCurrent() ?>
     <fieldset class="fieldset_band"
               id="band_<?=$lineupIndex?>"
@@ -19,7 +19,7 @@ $lineupIndex = 1;
         <label for="first_sign_<?=$lineupIndex?>" class="screen_reader_only">First letter the of the band name</label>
         <select  id="first_sign_<?=$lineupIndex?>"
                  autocomplete="off">
-            <?php foreach ($alphabet as $firstSign): ?>
+            <?php foreach ($alphabet as $firstSign) : ?>
                 <?php if ($gig->getBandFirstChar()->get() == $firstSign) : ?>
                     <option value="<?=$firstSign?>" selected><?=$firstSign?></option>
                 <?php else : ?>
@@ -41,25 +41,25 @@ $lineupIndex = 1;
             id="addition_<?=$lineupIndex?>"
             class="edit_field"
             placeholder="Extra information">
-        <button type="button" onclick="add_band_lineup(\'%2$u\')">
+        <button type="button" id="button_add_<?=$lineupIndex?>">
             <img src="<?=$this->get('imagePath')?>plus_small.png"
                  alt="Add band to lineup"
                  width="15"
                  height="15">
         </button>
-        <button type="button" onclick="del_band_lineup(\'%1$u\')">
+        <button type="button" id="button_delete_<?=$lineupIndex?>">
             <img src="<?=$this->get('imagePath')?>minus_small.png"
                  alt="Remove band <?=$lineupIndex?> from lineup"
                  width="15"
                  height="15">
         </button>
-        <button type="button" onclick="shift_band_lineup(\'%1$u\', \'up\')">
+        <button type="button" id="button_shift_up_<?=$lineupIndex?>">
             <img src="<?=$this->get('imagePath')?>arrow_up_small.png"
                  alt="Shift <?=$lineupIndex?>. band one line up"
                  width="15"
                  height="15">
         </button>
-        <button type="button" onclick="shift_band_lineup(\'%1$u\', \'down\')">
+        <button type="button" id="button_shift_down_<?=$lineupIndex?>">
             <img src="<?=$this->get('imagePath')?>arrow_down_small.png"
                  alt="Shift <?=$lineupIndex?>. Band one line down"
                  width="15"
