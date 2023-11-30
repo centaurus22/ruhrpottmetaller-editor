2fdd912056ec3c7dc7f1b48273ae98982411656d
Klaus Thorres <thorres@brothersofgrey.net>
2023-11-30 17:49:58 +0100

    Style the event editor

--- templates/main/editor.inc.php
+++ templates/main/editor.inc.php
@@ -4,47 +4,49 @@
     <input type="hidden" name="id" value="<?=$this->get('event')->getId()?>">
     <fieldset class="fieldset_general">
         <legend>General concert data</legend>
-
-        <label for="name" class="edit_label">Name</label>
-        <input type="text"
-            name="name"
-            id="name"
-            value="<?=$this->get('event')->getName()?>"
-            class="edit_text"
-            placeholder="Name of the concert"
-        >
-        <br>
-        <label for="date_start" class="edit_label">Date*</label>
-        <input type="date"
-            name="date_start"
-            id="date_start"
-            value="<?=$this->get('event')->getDate()->getFormatted() ?>"
-            required class="edit_date"
-        >
-        <span aria-hidden="true">&nbsp;for&nbsp;</span>
-        <label class="screenreader_only" for="length">Number of days</label>
-        <input type="number"
-            name="length"
-            id="length"
-            value="<?=$this->get('event')->getNumberOfDays()?>"
-            class="edit_length"
-            min="1"
-        >
-        <span aria-hidden="true">&nbsp;day(s)</span>
-        <br>
-        <span id="ajax_city_venue"
-              data-venue-id="<?=$this->get('event')->getVenueId() ?? null ?>"
-              data-city-id="<?=$this->get('event')->getCityId()  ?? null ?>">
-        </span><br>
-        <label for="url" class="edit_label">URL*</label>
-        <input type="url"
-            name="url"
-            id="url"
-            class="edit_text"
-            value="<?=$this->get('event')->getUrl()?>"
-            placeholder="Link to more information"
-            required
-        >
+        <div>
+            <div>
+                <label for="name" class="edit_label">Name</label>
+                <input type="text"
+                    name="name"
+                    id="name"
+                    value="<?=$this->get('event')->getName()?>"
+                    class="edit_text"
+                    placeholder="Name of the concert">
+            </div>
+            <div>
+                <label for="date_start" class="edit_label">Date*</label>
+                <input type="date"
+                    name="date_start"
+                    id="date_start"
+                    value="<?=$this->get('event')->getDate()->getFormatted() ?>"
+                    class="edit_date"
+                    required>
+                <span aria-hidden="true">&nbsp;for&nbsp;</span>
+                <label class="screen_reader_only" for="length">Number of days</label>
+                <input type="number"
+                    name="length"
+                    id="length"
+                    value="<?=$this->get('event')->getNumberOfDays()?>"
+                    class="edit_length"
+                    min="1">
+                <span aria-hidden="true">&nbsp;day(s)</span>
+            </div>
+            <div id="ajax_city_venue"
+                 data-venue-id="<?=$this->get('event')->getVenueId() ?? null ?>"
+                 data-city-id="<?=$this->get('event')->getCityId()  ?? null ?>">
+            </div>
+            <div>
+                <label for="url" class="edit_label">URL*</label>
+                <input type="url"
+                    name="url"
+                    id="url"
+                    class="edit_text"
+                    value="<?=$this->get('event')->getUrl()?>"
+                    placeholder="Link to more information"
+                    required>
+            </div>
+        </div>
     </fieldset>
     <fieldset id="ajax_lineup" data-event-id="<?=$this->get('event')->getId()?>">
     </fieldset>
