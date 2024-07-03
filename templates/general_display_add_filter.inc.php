cee79ffea91f65c2578528e3a09c6a3d519605b5
Klaus Quintern <klaus.quintern@violution.net>
2024-07-03 08:19:51 +0200

    Delete old Classes and template

--- templates/general_display_add_filter.inc.php
+++
@@ -1,6 +0,0 @@
-<main id="main">
-    <?=$this->_['filter_value_changer']; ?>
-    <div class="content content_small">
-        Please select a filter value in the dropdown menu at the top left.
-    </div>
-</main>
